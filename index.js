dafb967bf33c423f09c96bfbeb43df3f4bb851d4
Sandeep Mistry <sandeep.mistry@gmail.com>
2013-11-24 14:09:27 -0500

    - read system id, serial number, firmware/hardware revision, and battery level

--- index.js
+++ index.js
@@ -3,7 +3,14 @@ var util = require('util');
 
 var noble = require('noble');
 
-var SERVICE_UUID = '39e1fa0084a811e2afba0002a5d5c51b';
+var SERVICE_UUID                            = '39e1fa0084a811e2afba0002a5d5c51b';
+
+var SYSTEM_ID_UUID                          = '2a23';
+var SERIAL_NUMBER_UUID                      = '2a25';
+var FIRMWARE_REVISION_UUID                  = '2a26';
+var HARDWARE_REVISION_UUID                  = '2a27';
+
+var BATTERY_LEVEL_UUID                      = '2a19';
 
 function FlowerPower(peripheral) {
   this._peripheral = peripheral;
@@ -78,6 +85,69 @@ FlowerPower.prototype.discoverServicesAndCharacteristics = function(callback) {
 
     callback(error);
   }.bind(this));
+};
+
+FlowerPower.prototype.writeCharacteristic = function(uuid, data, callback) {
+  this._characteristics[uuid].write(data, false, callback);
+};
+
+FlowerPower.prototype.notifyCharacteristic = function(uuid, notify, listener, callback) {
+  var characteristic = this._characteristics[uuid];
+
+  characteristic.notify(notify, function(state) {
+    if (notify) {
+      characteristic.addListener('read', listener);
+    } else {
+      characteristic.removeListener('read', listener);
+    }
+
+    callback();
+  });
+};
+
+FlowerPower.prototype.readDataCharacteristic = function(uuid, callback) {
+  this._characteristics[uuid].read(function(error, data) {
+    callback(data);
+  });
+};
+
+FlowerPower.prototype.readStringCharacteristic = function(uuid, callback) {
+  this.readDataCharacteristic(uuid, function(data) {
+    for (var i = 0; i < data.length; i++) {
+      if (data[i] === 0x00) {
+        data = data.slice(0, i);
+        break;
+      }
+    }
+
+    callback(data.toString());
+  });
+};
+
+FlowerPower.prototype.readSystemId = function(callback) {
+  this.readDataCharacteristic(SYSTEM_ID_UUID, function(data) {
+    var systemId = data.toString('hex').match(/.{1,2}/g).reverse().join(':');
+
+    callback(systemId);
+  });
+};
+
+FlowerPower.prototype.readSerialNumber = function(callback) {
+  this.readStringCharacteristic(SERIAL_NUMBER_UUID, callback);
+};
+
+FlowerPower.prototype.readFirmwareRevision = function(callback) {
+  this.readStringCharacteristic(FIRMWARE_REVISION_UUID, callback);
+};
+
+FlowerPower.prototype.readHardwareRevision = function(callback) {
+  this.readStringCharacteristic(HARDWARE_REVISION_UUID, callback);
+};
+
+FlowerPower.prototype.readBatteryLevel = function(callback) {
+  this.readDataCharacteristic(BATTERY_LEVEL_UUID, function(data) {
+    callback(data.readUInt8(0));
+  });
 };
 
 module.exports = FlowerPower;
--- test.js
+++ test.js
@@ -18,6 +18,42 @@ FlowerPower.discover(function(flowerPower) {
       flowerPower.discoverServicesAndCharacteristics(callback);
     },
     function(callback) {
+      console.log('readSystemId');
+      flowerPower.readSystemId(function(systemId) {
+        console.log('\tsystem id = ' + systemId);
+        callback();
+      });
+    },
+    function(callback) {
+      console.log('readSerialNumber');
+      flowerPower.readSerialNumber(function(serialNumber) {
+        console.log('\tserial number = ' + serialNumber);
+        callback();
+      });
+    },
+    function(callback) {
+      console.log('readFirmwareRevision');
+      flowerPower.readFirmwareRevision(function(firmwareRevision) {
+        console.log('\tfirmware revision = ' + firmwareRevision);
+        callback();
+      });
+    },
+    function(callback) {
+      console.log('readHardwareRevision');
+      flowerPower.readHardwareRevision(function(hardwareRevision) {
+        console.log('\thardware revision = ' + hardwareRevision);
+        callback();
+      });
+    },
+    function(callback) {
+      console.log('readBatteryLevel');
+      flowerPower.readBatteryLevel(function(batteryLevel) {
+        console.log('battery level = ' + batteryLevel);
+
+        callback();
+      });
+    },
+    function(callback) {
       console.log('disconnect');
       flowerPower.disconnect(callback);
     }
